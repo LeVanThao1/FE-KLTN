9b00236f4b1c0a4fee6f9d17999bf684de478c24
Lê Văn Thảo <thaolv210402@gmail.com>
2021-04-28 02:15:53 +0700

    done search

--- src/store/category.js
+++ src/store/category.js
@@ -4,13 +4,29 @@ export class Category {
   @observable
   categories = undefined;
 
-  constructor(categories) {
+  @observable
+  selectCategory = undefined;
+
+  @observable
+  option = undefined;
+
+  constructor(categories, selectCategory, option) {
     this.categories = categories;
+    this.selectCategory = selectCategory;
+    this.option = option;
     makeObservable(this);
   }
 
   @action
   setCategory = (value) => {
     this.categories = value;
+  };
+  @action
+  setOption = (value) => {
+    this.option = value;
+  };
+  @action
+  setSelectCategory = (value) => {
+    this.selectCategory = value;
   };
 }
